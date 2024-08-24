3c2de81dfd38f988af621744d8237429322cf023
asharab <codewithasharab@gmail.com>
2024-08-24 10:05:06 +0500

    "software updated"

--- lib/main.dart
+++ lib/main.dart
@@ -40,22 +40,31 @@ class _MyHomePageState extends State<MyHomePage> {
 
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
+       backgroundColor: Colors.yellow,
         title: Text("Calculator"),
       ),
-      body: Center(
-        child: Container(
-            width: 500,
-            height: 400,
-            color: Colors.blue,
+      body:Container(
+            width: 390,
+            height: 800,
+            decoration: BoxDecoration(
+              border:Border.all(
+                width: 2,
+                color: Colors.black,
+              ),
+              gradient: LinearGradient(
+
+                stops: [0.1,0.6,],
+                colors: [
+                Colors.yellow,
+                Colors.red,
+
+              ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+            ),
                 child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.end,
                   children: [
                     Row(
                       children: [
@@ -63,202 +72,207 @@ class _MyHomePageState extends State<MyHomePage> {
                           margin: EdgeInsets.all(20),
                           width: 200,
                           height: 40,
-                          color: Colors.white,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(11)
+                          ),
+                          
                         ),
                         ElevatedButton(
-                          child: Text("Clear",style: TextStyle(color: Colors.black,fontSize: 25,fontWeight: FontWeight.w200),),
+                          child: Text("Clear",style: TextStyle(color: Colors.black,fontSize: 25,fontWeight: FontWeight.w300),),
                           onPressed: (){},
                         )
                       ],
                     ),
                     Row(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      mainAxisAlignment: MainAxisAlignment.end,
                       children: [
                         Container(
                           margin: EdgeInsets.all(10),
-                          width: 100,
-                          height: 40,
-                          color: Colors.orangeAccent,
-                          child: InkWell(
-                            child: Center(child: Text("1",style: TextStyle(fontWeight: FontWeight.w300,fontSize: 30),)),
-                            onTap: (){},
+                          width: 70,
+                          height: 60,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.grey,
                           ),
+
+                         child: Center(
+                             child: Text("1",style: TextStyle(fontWeight: FontWeight.w300,fontSize: 30),)),
+                        ),
+
+                        Container(
+                          margin: EdgeInsets.all(10),
+                          width: 70,
+                          height: 60,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.grey,
+                          ),child: Center(child: Text("2",style: TextStyle(fontWeight: FontWeight.w300,fontSize: 30),),),
 
                         ),
                         Container(
                           margin: EdgeInsets.all(10),
-                          width: 100,
-                          height: 40,
-                          color: Colors.orangeAccent,
-                          child: InkWell(
-                            child: Center(child: Text("2",style: TextStyle(fontWeight: FontWeight.w300,fontSize: 30),)),
-                            onTap: (){},
-                          ),
+                          width: 70,
+                          height: 60,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.grey,
+                          ),child: Center(child: Text("3",style: TextStyle(fontWeight: FontWeight.w300,fontSize: 30),),),
 
                         ),
                         Container(
-                          margin: EdgeInsets.all(10),
-                          width: 100,
-                          height: 40,
-                          color: Colors.orangeAccent,
-                          child: InkWell(
-                            child: Center(child: Text("3",style: TextStyle(fontWeight: FontWeight.w300,fontSize: 30),)),
-                            onTap: (){},
-                          ),
-
-                        ),
-                        Container(
-                          margin: EdgeInsets.all(10),
-                          width: 100,
-                          height: 40,
-                          color: Colors.orangeAccent,
-                          child: InkWell(
-                            child: Center(child: Text("+",style: TextStyle(fontWeight: FontWeight.w300,fontSize: 30),)),
-                            onTap: (){},
-                          ),
+                          margin: EdgeInsets.only(top: 10,right: 10,bottom: 10),
+                          width: 70,
+                          height: 60,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.orange,
+                          ),child: Center(child: Text("+",style: TextStyle(fontWeight: FontWeight.w300,fontSize: 30),),),
 
                         ),
                       ],
                     ),
-                    Row(
+                    Row( crossAxisAlignment: CrossAxisAlignment.end,
+                      mainAxisAlignment: MainAxisAlignment.end,
                       children: [
                         Container(
                           margin: EdgeInsets.all(10),
-                          width: 100,
-                          height: 40,
-                          color: Colors.orangeAccent,
-                          child: InkWell(
-                            child: Center(child: Text("4",style: TextStyle(fontWeight: FontWeight.w300,fontSize: 30),)),
-                            onTap: (){},
-                          ),
+                          width: 70,
+                          height: 60,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.grey,
+                          ),child: Center(child: Text("4",style: TextStyle(fontWeight: FontWeight.w300,fontSize: 30),),),
 
                         ),
                         Container(
                           margin: EdgeInsets.all(10),
-                          width: 100,
-                          height: 40,
-                          color: Colors.orangeAccent,
-                          child: InkWell(
-                            child: Center(child: Text("5",style: TextStyle(fontWeight: FontWeight.w300,fontSize: 30),)),
-                            onTap: (){},
-                          ),
+                          width: 70,
+                          height: 60,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.grey,
+                          ),child: Center(child: Text("5",style: TextStyle(fontWeight: FontWeight.w300,fontSize: 30),),),
 
                         ),
                         Container(
                           margin: EdgeInsets.all(10),
-                          width: 100,
-                          height: 40,
-                          color: Colors.orangeAccent,
-                          child: InkWell(
-                            child: Center(child: Text("6",style: TextStyle(fontWeight: FontWeight.w300,fontSize: 30),)),
-                            onTap: (){},
-                          ),
+                          width: 70,
+                          height: 60,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.grey,
+                          ),child: Center(child: Text("6",style: TextStyle(fontWeight: FontWeight.w300,fontSize: 30),),),
 
                         ),
                         Container(
-                          margin: EdgeInsets.all(10),
-                          width: 100,
-                          height: 40,
-                          color: Colors.orangeAccent,
-                          child: InkWell(
-                            child: Center(child: Text("-",style: TextStyle(fontWeight: FontWeight.w300,fontSize: 30),)),
-                            onTap: (){},
-                          ),
+                          margin: EdgeInsets.only(top: 10,right: 10,bottom: 10),
+                          width: 70,
+                          height: 60,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.orange,
+                          ),child: Center(child: Text("-",style: TextStyle(fontWeight: FontWeight.w300,fontSize: 30),),),
+
 
                         ),
                       ],
-                    ),Row(
+                    ),
+                    Row( crossAxisAlignment: CrossAxisAlignment.end,
+                      mainAxisAlignment: MainAxisAlignment.end,
                       children: [
                         Container(
                           margin: EdgeInsets.all(10),
-                          width: 100,
-                          height: 40,
-                          color: Colors.orangeAccent,
-                          child: InkWell(
-                            child: Center(child: Text("7",style: TextStyle(fontWeight: FontWeight.w300,fontSize: 30),)),
-                            onTap: (){},
-                          ),
+                          width: 70,
+                          height: 60,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.grey,
+                          ),child: Center(child: Text("7",style: TextStyle(fontWeight: FontWeight.w300,fontSize: 30),),),
 
                         ),
                         Container(
                           margin: EdgeInsets.all(10),
-                          width: 100,
-                          height: 40,
-                          color: Colors.orangeAccent,
-                          child: InkWell(
-                            child: Center(child: Text("8",style: TextStyle(fontWeight: FontWeight.w300,fontSize: 30),)),
-                            onTap: (){},
-                          ),
+                          width: 70,
+                          height: 60,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.grey,
+                          ),child: Center(child: Text("8",style: TextStyle(fontWeight: FontWeight.w300,fontSize: 30),),),
+
 
                         ),
                         Container(
                           margin: EdgeInsets.all(10),
-                          width: 100,
-                          height: 40,
-                          color: Colors.orangeAccent,
-                          child: InkWell(
-                            child: Center(child: Text("9",style: TextStyle(fontWeight: FontWeight.w300,fontSize: 30),)),
-                            onTap: (){},
-                          ),
+                          width: 70,
+                          height: 60,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.grey,
+                          ),child: Center(child: Text("9",style: TextStyle(fontWeight: FontWeight.w300,fontSize: 30),),),
+
 
                         ),
                         Container(
-                          margin: EdgeInsets.all(10),
-                          width: 100,
-                          height: 40,
-                          color: Colors.orangeAccent,
-                          child: InkWell(
-                            child: Center(child: Text("x",style: TextStyle(fontWeight: FontWeight.w300,fontSize: 30),)),
-                            onTap: (){},
-                          ),
+                          margin: EdgeInsets.only(top: 10,right: 10,bottom: 10),
+                          width: 70,
+                          height: 60,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.orange,
+                          ),child: Center(child: Text("x",style: TextStyle(fontWeight: FontWeight.w300,fontSize: 30),),),
+
 
                         ),
                       ],
-                    ),Row(
+                    ),
+                    Row( crossAxisAlignment: CrossAxisAlignment.end,
+                      mainAxisAlignment: MainAxisAlignment.end,
                       children: [
                         Container(
                           margin: EdgeInsets.all(10),
-                          width: 100,
-                          height: 40,
-                          color: Colors.orangeAccent,
-                          child: InkWell(
-                            child: Center(child: Text(".",style: TextStyle(fontWeight: FontWeight.w300,fontSize: 30),)),
-                            onTap: (){},
+                          width: 70,
+                          height: 60,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.grey,
                           ),
+                          child: Center(child: Text(".",style: TextStyle(fontWeight: FontWeight.w300,fontSize: 30),),),
 
                         ),
                         Container(
                           margin: EdgeInsets.all(10),
-                          width: 100,
-                          height: 40,
-                          color: Colors.orangeAccent,
-                          child: InkWell(
-                            child: Center(child: Text("9",style: TextStyle(fontWeight: FontWeight.w300,fontSize: 30),)),
-                            onTap: (){},
-                          ),
+                          width: 70,
+                          height: 60,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.grey,
+                          ),child: Center(child: Text("0",style: TextStyle(fontWeight: FontWeight.w300,fontSize: 30),),),
+
 
                         ),
                         Container(
                           margin: EdgeInsets.all(10),
-                          width: 100,
-                          height: 40,
-                          color: Colors.orangeAccent,
-                          child: InkWell(
-                            child: Center(child: Text("=",style: TextStyle(fontWeight: FontWeight.w300,fontSize: 30),)),
-                            onTap: (){},
-                          ),
+                          width: 70,
+                          height: 60,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.grey,
+                          ),child: Center(child: Text("=",style: TextStyle(fontWeight: FontWeight.w300,fontSize: 30),),),
+
 
                         ),
                         Container(
-                          margin: EdgeInsets.all(10),
-                          width: 100,
-                          height: 40,
-                          color: Colors.orangeAccent,
-                          child: InkWell(
-                            child: Center(child: Text("/",style: TextStyle(fontWeight: FontWeight.w300,fontSize: 30),)),
-                            onTap: (){},
-                          ),
+                          margin: EdgeInsets.only(top: 10,right: 10,bottom: 10),
+                          width: 70,
+                          height: 60,
+                             decoration: BoxDecoration(
+                             shape: BoxShape.circle,
+                          color: Colors.orange,
+                            ),child: Center(child: Text("/",style: TextStyle(fontWeight: FontWeight.w300,fontSize: 30),),),
 
-                        ),
+    ),
                       ],
                     ),
 
@@ -266,8 +280,6 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
 
             ),
-      ),//Display area
-
     );
   }
 }
